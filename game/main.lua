377bafae8b898741851612102fbe91ec62f5cb3e
れみろう <remyroez+github@gmail.com>
2019-05-04 13:12:53 +0900

    :sparkles: 必要最低限のソースを追加

---
+++ game/main.lua
@@ -0,0 +1,79 @@
+
+-- グローバルに影響があるライブラリ
+require 'autobatch'
+require 'sbss'
+
+-- シーンステート
+local scenes = require 'scenes'
+
+-- クラス
+local Scene = require 'Scene'
+
+-- シーン
+local scene = Scene()
+scene:gotoState 'boot'
+
+-- デバッグモード
+local debugMode = false
+
+-- 画面のサイズ
+local width, height = 0, 0
+
+-- 読み込み
+function love.load()
+    love.math.setRandomSeed(love.timer.getTime())
+    width, height = love.graphics.getDimensions()
+end
+
+-- 更新
+function love.update(dt)
+    -- シーンの更新
+    scene:update(dt)
+end
+
+-- 描画
+function love.draw()
+    -- 画面のリセット
+    love.graphics.reset()
+
+    -- シーンの描画
+    scene:draw()
+
+    -- ステートの描画
+    if debugMode then
+        love.graphics.setColor(1, 1, 1)
+        scene:printStates(0, 0)
+    end
+end
+
+-- キー入力
+function love.keypressed(key, scancode, isrepeat)
+    if key == 'escape' then
+        -- 終了
+        love.event.quit()
+    elseif key == 'printscreen' then
+        -- スクリーンショット
+        love.graphics.captureScreenshot(os.time() .. ".png")
+    elseif key == 'f1' then
+        -- スキャン
+        lurker.scan()
+    elseif key == 'f5' then
+        -- リスタート
+        love.event.quit('restart')
+    elseif key == 'f12' then
+        -- デバッグモード切り替え
+        debugMode = not debugMode
+
+        -- シーンに反映
+        scene:setDebugMode(debugMode)
+    else
+        -- シーンに処理を渡す
+        scene:keypressed(key, scancode, isrepeat)
+    end
+end
+
+-- マウス入力
+function love.mousepressed(...)
+    -- シーンに処理を渡す
+    scene:mousepressed(...)
+end
